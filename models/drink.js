d7dbcad712f8ae7b0aac7fe6255c2a72ae7d507f
JosephBizra <j.bizra@gmail.com>
2019-07-21 17:07:06 -0500

    another commit

--- models/drink.js
+++ models/drink.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
+  var Drink = sequelize.define("Drink", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
-  return Example;
+  return Drink;
 };
